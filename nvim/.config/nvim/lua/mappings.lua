6615072c82695e882013667b29498861cb6e35c2
minhngoncoding <dangminhngon.work@gmail.com>
2026-03-19 08:42:40 +0700

    code_runner: saving before run code

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,9 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>", {desc = "Exit Insert mode"})
 
 -- Code Runner mappings
-map("n", "<leader>rr", ":RunCode<CR>", { desc = "Run Code" })
-map("n", "<leader>rf", ":RunFile<CR>", { desc = "Run Current File" })
-map("n", "<leader>rp", ":RunProject<CR>", { desc = "Run Project" })
+map("n", "<leader>rr", ":w | RunCode<CR>", { desc = "Run Code" })
+map("n", "<leader>rf", ":w | RunFile<CR>", { desc = "Run Current File" })
+map("n", "<leader>rp", ":w | RunProject<CR>", { desc = "Run Project" })
 map("n", "<leader>rc", ":RunClose<CR>", { desc = "Close Runner" })
 
 --  Toggle Markdown
