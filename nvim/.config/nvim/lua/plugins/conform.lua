6539aea46742267c24857eb29a8961dc7ecb1451
minhngoncoding <dangminhngon.work@gmail.com>
2026-03-20 08:52:58 +0700

    Fixing ruff_fix auto delete unused import and variables

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,34 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    -- Everything goes inside here
-    format_on_save = {
-      lsp_fallback = true,
-      timeout_ms = 500, -- Increased slightly for stability
-    },
-    formatters_by_ft = {
-      css = { "prettier" },
-      html = { "prettier" },
-      lua = { "stylua" },
-      python = { "ruff_fix", "ruff_format" },
-    },
-  },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		-- Everything goes inside here
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500, -- Increased slightly for stability
+		},
+		formatters_by_ft = {
+			css = { "prettier" },
+			html = { "prettier" },
+			lua = { "stylua" },
+			python = { "ruff_fix_safe", "ruff_format" },
+		},
+		formatters = {
+			ruff_fix_safe = {
+				command = "ruff",
+				args = {
+					"check",
+					"--fix",
+					"--unfixable",
+					"F401,F841",
+					"--force-exclude",
+					"--exit-zero",
+					"--no-cache",
+					"--stdin-filename",
+					"$FILENAME",
+					"-",
+				},
+			},
+		},
+	},
 }
