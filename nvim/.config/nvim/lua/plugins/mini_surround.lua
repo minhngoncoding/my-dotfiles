00e7c4be3b9ac52f48515068fe8794acc09207b3
minhngoncoding <dangminhngon.work@gmail.com>
2026-03-21 09:16:52 +0700

    Fix mini.surround conflict with substitue key (s).

--- nvim/.config/nvim/lua/plugins/mini_surround.lua
+++ nvim/.config/nvim/lua/plugins/mini_surround.lua
@@ -1,14 +1,17 @@
 return {
 	"echasnovski/mini.surround",
+	-- Note: We use 'keys' to lazy-load the plugin only when you actually use it
+	keys = {
+		{ "sa", desc = "Add surrounding", mode = { "n", "v" } },
+		{ "sd", desc = "Delete surrounding" },
+		{ "sr", desc = "Replace surrounding" },
+		{ "sf", desc = "Find surrounding (right)" },
+		{ "sF", desc = "Find surrounding (left)" },
+		{ "sh", desc = "Highlight surrounding" },
+		{ "sn", desc = "Update n_lines" },
+	},
 	opts = {
-		mappings = {
-			add = "sa", -- Add surrounding
-			delete = "sd", -- Delete surrounding
-			replace = "sr", -- Replace surrounding
-			find = "sf", -- Find surrounding (to the right)
-			find_left = "sF", -- Find surrounding (to the left)
-			highlight = "sh", -- Highlight surrounding
-			update_n_lines = "sn", -- Update `n_lines`
-		},
+		-- This helps find surroundings even if you aren't "inside" them yet
+		search_method = "cover_or_next",
 	},
 }
